fdac1fc730e9b05fc1ad74bb3bb0da75357d287e
Ankit Yadav <sy19072003@gmail.com>
2024-09-29 18:33:15 +0530

    new

--- download_file.py
+++ download_file.py
@@ -1,20 +1,5 @@
 
-# from flask import jsonify, send_file
-# from database import get_database_session, File
-# import os
 
-# def download_file(file_id):
-#     session = get_database_session()
-    
-#     file = session.query(File).filter_by(id=file_id).first()
-#     if not file:
-#         return jsonify({"message": "File not found"}), 404
-    
-#     file_path = file.file_url
-#     if not os.path.exists(file_path):
-#         return jsonify({"message": "File does not exist on server"}), 404
-    
-#     return send_file(file_path, as_attachment=True)
 from flask import request, jsonify, send_file
 from database import get_database_session, File
 import os
